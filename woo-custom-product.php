3b6a3da5f4a81a8334dfe81440f569c355189d19
hannannexus <hannannexus@gmail.com>
2023-03-03 10:32:23 +0600

    Inital Commit

---
+++ woo-custom-product.php
@@ -0,0 +1,16 @@
+
+<?php 
+
+/** 
+ *  Plugin Name: Woo Custom Product 
+ *  Description: Add custom product type in woocommerce
+ *  Plugin URL: 
+ *  Author: Hannan
+ *  Author URL: 
+ *  Version: 1.0.0
+ *  Text Domain: woo-custom-product
+ * 
+ * 
+*/
+
+
