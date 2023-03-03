7cfe72dc389371fc57cf2f57fa7a493488250068
hannannexus <hannannexus@gmail.com>
2023-03-03 12:50:04 +0600

    added custom product type and tabs

--- woo-custom-product.php
+++ woo-custom-product.php
@@ -4,9 +4,9 @@
 /** 
  *  Plugin Name: Woo Custom Product 
  *  Description: Add custom product type in woocommerce
- *  Plugin URL: 
+ *  Plugin URL: https://github.com/hannannexus/
  *  Author: Hannan
- *  Author URL: 
+ *  Author URL:  https://github.com/hannannexus/
  *  Version: 1.0.0
  *  Text Domain: woo-custom-product
  * 
@@ -14,3 +14,47 @@
 */
 
 
+/** 
+ *  register custom prduct trype for woocommerce 
+*/
+
+ function register_car_product_type(){
+    class WC_Product_Car extends WC_Product{
+
+        public function __construct($product){
+            $this->product_type = "car";           
+            parent:: __construct($product);
+        }
+        
+    }
+}
+add_action('init','register_car_product_type');
+
+
+/** 
+ *  Add csutom Product type to this product dropdown menu
+*/
+
+function add_car_product_type($types){
+    $types["car"] = __("Car","woo-custom-product");
+
+    return $types;
+}
+add_filter('product_type_selector','add_car_product_type');
+
+
+/** 
+ *  Add  new Tab for car product 
+*/
+
+function car_data_tabs( $tabs ){
+
+    $tabs['car'] = array(
+        'label' => __(' Product Car','woo-custom-product'),
+        'target' => 'car_product_option',
+        'class' => array('show_if_car_product'),
+        'priority' => 10
+    );    
+    return $tabs;
+}
+add_filter('woocommerce_product_data_tabs','car_data_tabs');
